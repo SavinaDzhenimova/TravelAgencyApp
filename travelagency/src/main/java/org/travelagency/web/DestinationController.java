6f09bc05e765c80c51ff3786a052f3c3ff478460
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-02-09 12:51:14 +0200

    Added add-destination page

--- travelagency/src/main/java/org/travelagency/web/DestinationController.java
+++ travelagency/src/main/java/org/travelagency/web/DestinationController.java
@@ -1,12 +1,15 @@
 package org.travelagency.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.travelagency.model.exportDTO.DestinationViewDTO;
 import org.travelagency.model.exportDTO.DestinationViewInfo;
+import org.travelagency.model.importDTO.AddCandidateDTO;
+import org.travelagency.model.importDTO.AddDestinationDTO;
 import org.travelagency.service.interfaces.DestinationService;
 
 @Controller
@@ -28,5 +31,15 @@ public class DestinationController {
         modelAndView.addObject("destination", destinationViewInfo);
 
         return modelAndView;
+    }
+
+    @GetMapping("/add-destination")
+    public ModelAndView addDestination(Model model) {
+
+        if (!model.containsAttribute("addDestinationDTO")) {
+            model.addAttribute("addDestinationDTO", new AddDestinationDTO());
+        }
+
+        return new ModelAndView("add-destination");
     }
 }
