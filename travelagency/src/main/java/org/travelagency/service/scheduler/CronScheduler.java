5ba149b812ca10a9d37cfb9454ec516bed92deef
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-03-03 20:16:28 +0200

    Added cron scheduler

--- travelagency/src/main/java/org/travelagency/service/scheduler/CronScheduler.java
+++ travelagency/src/main/java/org/travelagency/service/scheduler/CronScheduler.java
@@ -5,23 +5,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.travelagency.model.entity.Excursion;
+import org.travelagency.model.entity.Reservation;
 import org.travelagency.repository.ExcursionRepository;
+import org.travelagency.repository.ReservationRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class ExcursionScheduler {
+public class CronScheduler {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(ExcursionScheduler.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(CronScheduler.class);
     private final ExcursionRepository excursionRepository;
+    private final ReservationRepository reservationRepository;
 
-    public ExcursionScheduler(ExcursionRepository excursionRepository) {
+    public CronScheduler(ExcursionRepository excursionRepository, ReservationRepository reservationRepository) {
         this.excursionRepository = excursionRepository;
+        this.reservationRepository = reservationRepository;
     }
 
-    @Scheduled(cron = "0 0 3 * * *") // Всяка нощ в 03:00
+    @Scheduled(cron = "0 0 4 * * *") // Всяка нощ в 04:00
     public void deletePastDates() {
         List<Excursion> excursions = this.excursionRepository.findAll();
         LocalDate currentDate = LocalDate.now();
@@ -47,5 +51,21 @@ public class ExcursionScheduler {
         }
 
         LOGGER.info("Checked and updated excursions with past dates.");
+    }
+
+    @Scheduled(cron = "0 0 3 * * *") // Всяка нощ в 03:00
+    public void deletePastReservations() {
+        List<Reservation> reservations = this.reservationRepository.findAll();
+        LocalDate currentDate = LocalDate.now();
+
+        for (Reservation reservation : reservations) {
+            if (reservation.getDate().isBefore(currentDate)) {
+                this.reservationRepository.delete(reservation);
+
+                LOGGER.info("Deleted reservation with ID: {} because the date is in the past.", reservation.getId());
+            }
+        }
+
+        LOGGER.info("Checked and deleted reservations with past dates.");
     }
 }
