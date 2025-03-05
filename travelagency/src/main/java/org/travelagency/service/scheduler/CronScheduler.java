107b9dcf589da97232abdfff55716b98b08d4b3f
Savina Dzhenimova <129115347+SavinaDzhenimova@users.noreply.github.com>
2025-03-05 20:05:49 +0200

    Small changes in Cron scheduler

--- travelagency/src/main/java/org/travelagency/service/scheduler/CronScheduler.java
+++ travelagency/src/main/java/org/travelagency/service/scheduler/CronScheduler.java
@@ -8,7 +8,9 @@ import org.travelagency.model.entity.Excursion;
 import org.travelagency.model.entity.Reservation;
 import org.travelagency.repository.ExcursionRepository;
 import org.travelagency.repository.ReservationRepository;
+import org.travelagency.service.utils.ExcursionDeletionManager;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,14 +21,17 @@ public class CronScheduler {
     private final Logger LOGGER = LoggerFactory.getLogger(CronScheduler.class);
     private final ExcursionRepository excursionRepository;
     private final ReservationRepository reservationRepository;
+    private final ExcursionDeletionManager excursionDeletionManager;
 
-    public CronScheduler(ExcursionRepository excursionRepository, ReservationRepository reservationRepository) {
+    public CronScheduler(ExcursionRepository excursionRepository, ReservationRepository reservationRepository,
+                         ExcursionDeletionManager excursionDeletionManager) {
         this.excursionRepository = excursionRepository;
         this.reservationRepository = reservationRepository;
+        this.excursionDeletionManager = excursionDeletionManager;
     }
 
     @Scheduled(cron = "0 0 4 * * *") // Всяка нощ в 04:00
-    public void deletePastDates() {
+    public void deletePastDates() throws IOException {
         List<Excursion> excursions = this.excursionRepository.findAll();
         LocalDate currentDate = LocalDate.now();
 
@@ -38,7 +43,7 @@ public class CronScheduler {
 
             if (excursion.getDates().size() != futureDates.size()) {
                 if (futureDates.isEmpty()) {
-                    this.excursionRepository.delete(excursion);
+                    this.excursionDeletionManager.deleteExcursion(excursion.getId());
 
                     LOGGER.info("Deleted excursion with ID: {} because all dates were in the past.", excursion.getId());
                 } else {
@@ -59,8 +64,8 @@ public class CronScheduler {
         LocalDate currentDate = LocalDate.now();
 
         for (Reservation reservation : reservations) {
-            if (reservation.getDate().isBefore(currentDate)) {
-                this.reservationRepository.delete(reservation);
+            if (reservation.getExcursionDate().isBefore(currentDate)) {
+                this.reservationRepository.deleteById(reservation.getId());
 
                 LOGGER.info("Deleted reservation with ID: {} because the date is in the past.", reservation.getId());
             }
